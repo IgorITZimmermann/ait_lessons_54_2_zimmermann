1a5189fab261631c5a3a1f1d1632fd379c74fb75
Zimmermann <igor.sonstnix@gmail.com>
2024-12-12 21:22:25 +0100

    #1: Unit-Test was implemented

--- src/main/java/de/ait/Main.java
+++ src/main/java/de/ait/Main.java
@@ -1,11 +1,8 @@
 package de.ait;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
 
-    public static  String getGreeting(String name) {
+    public  String getGreeting(String name) {
         return "Hello, " + name + "!";
     }
 }
